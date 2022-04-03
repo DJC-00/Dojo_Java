bce77cbbd606a84e751026f49cfa1163012bb4a5
DJC-00 <djcastle2172@gmail.com>
2022-04-03 14:47:41 -0500

    First Java Program

---
+++ Greetings/Hello.java
@@ -0,0 +1,13 @@
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("My name is DJ\nI am 28 years old\nMy hometown is Grove City Ohio");
+
+	}
+}
+
+
+    
+    
+
